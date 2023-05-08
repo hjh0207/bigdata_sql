35e9d96a4e33762a91d69b29f21fe214216682a4
hjh0207 a <h2j10207@gmail.com>
2023-05-08 07:01:50 +0000

    rly

--- dbdb.py
+++ dbdb.py
@@ -1,29 +1,37 @@
 import sqlite3
 
-# 연결
+# sqlite db 파일 생성 및 연결
 def db_con():
     return sqlite3.connect('dbdb.db')
 
-# 데이터 넣기
+# 데이터 넣기 함수
 def save_data():
     con = db_con()
+    # sql 문장을 실행시키기 위해 준비
     cursor = con.cursor()
     sql = '''
     INSERT INTO Person (ID, Name, Birthday)
-    VALUES (1, '이혜리', '1994-06-09');
+    VALUES (1, '이혜리', '1994-06-09')
     '''
     cursor.execute(sql) # sql 을 실행
-    con.commit()
-    con.close()
+    con.commit() # 적용
+    con.close()  # db닫기
 
-#데이터 보기 함수
+# 데이터 보기 함수
 def get_data():
+    con = db_con()
+    # sql 문장을 실행시키기 위해 준비
+    cursor = con.cursor() 
+
     sql = '''
-    SELECT * FROM Person;
+    SELECT * FROM Person
     '''
     cursor.execute(sql) # sql 을 실행
-    #data = cursor.fetchone()
-    #print(data)
+    # 하나의 데이터를 보기
+    # data = cursor.fetchone()
+    # print(data)
 
+    # 전체 데이터 보기
     all_data = cursor.fetchall()
     print(all_data)
+    con.close()  # db닫기
